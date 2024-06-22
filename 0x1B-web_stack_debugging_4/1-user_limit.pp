ad1e2c625e4a5c795e80626f89a832347e62e401
tristangms <nvisionfactory@gmail.com>
2024-06-22 08:21:53 +0200

    deferment removal

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,9 +0,0 @@
-# Configure the OS for 'holberton' to login and open a file without any error message 
-exec { 'hard limit':
-  command => "sed -i 's/5/5000/' /etc/security/limits.conf",
-  path    => '/bin'
-}
-exec { 'soft limit':
-  command => "sed -i 's/4/5000/' /etc/security/limits.conf",
-  path    => '/bin'
-}
