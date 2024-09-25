6a15ccaad8871ab7adeed1a79e705a0f3ce31ccb
tristangms <nvisionfactory@gmail.com>
2024-09-25 15:57:07 +0200

    Refactor top_ten and number_of_subscribers functions for improved readability and performance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit"""
-
+"""
+    Uses Reddit API to print the number of subscribers of a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Get the number of subscribers for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    if r.status_code != 200:
-            return 0
-    json = r.json()
-    data = json.get('data')
-    return data.get('subscribers', 0)
+
+    data = response.json().get("data")
+    num_subs = data.get("subscribers")
+
+    return num_subs
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit"""
-
+"""
+    Uses reddit API to get 10 hot posts
+"""
 import requests
 
+
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    """Get 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print(None)
         return
-    
-    try:
-        if response.text == '':
-            print(None)
-            return
-        
-        data = response.json().get('data', {}).get('children', [])
-        if not data:
-            print(None)
-            return
-        
-        for post in data:
-            print(post.get('data', {}).get('title'))
-    except ValueError:
-        print(None)
+
+    data = response.json().get("data").get("children")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
