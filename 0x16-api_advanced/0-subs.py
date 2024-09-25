4a990841f73426402cce43daea84b7ccc3fd50ec
Kgothatso Ntsane <nvisionfactory@gmail.com>
2024-09-25 14:39:26 +0000

    Refactor number_of_subscribers function to improve readability and error handling

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,10 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
         return subscribers
     else:
         return 0
+        
+
